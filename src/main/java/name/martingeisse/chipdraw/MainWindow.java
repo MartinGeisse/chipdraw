2a0a851a29a8436357979732a5953c667b4b6953
Martin Geisse <martingeisse@googlemail.com>
2019-10-30 19:22:45 +0100

    store version in central place

--- src/main/java/name/martingeisse/chipdraw/MainWindow.java
+++ src/main/java/name/martingeisse/chipdraw/MainWindow.java
@@ -146,7 +146,7 @@ public class MainWindow extends JFrame {
             builder.add("Corner Stitching Extractor", () -> CornerStitchingExtrator.extract(design));
             builder.addMenu("Help");
             builder.addExternalLink("Contents", "https://github.com/MartinGeisse/chipdraw/blob/master/doc/index.md"); // TODO link to commit for this version
-            builder.add("About", () -> JOptionPane.showMessageDialog(MainWindow.this, "Chipdraw by Martin Geisse"));
+            builder.add("About", () -> JOptionPane.showMessageDialog(MainWindow.this, About.ABOUT_TEXT));
             setJMenuBar(builder.build());
         }
 
