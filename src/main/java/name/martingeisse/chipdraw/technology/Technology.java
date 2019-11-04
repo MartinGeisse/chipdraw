5434f8e1d0ca2b60d1866233649b8d79a88c202d
Martin Geisse <martingeisse@googlemail.com>
2019-11-04 20:47:18 +0100

    separate layers from planes, WIP

--- src/main/java/name/martingeisse/chipdraw/technology/Technology.java
+++ src/main/java/name/martingeisse/chipdraw/technology/Technology.java
@@ -15,42 +15,103 @@ public final class Technology {
 
     private final String id;
     private final ImmutableList<PlaneSchema> planeSchemas;
+    private final int globalLayerCount;
+    private final int[] globalLayerIndexToPlaneIndex;
+    private final int[] globalLayerIndexToLocalLayerIndex;
 
     public Technology(String id, ImmutableList<PlaneSchema> planeSchemas) {
         this.id = id;
         this.planeSchemas = planeSchemas;
+
+        // make sure that none of the plane schemas is already in use
         for (PlaneSchema planeSchema : planeSchemas) {
             if (planeSchema.getIndex() >= 0) {
                 throw new IllegalArgumentException("plane schema is already used by a Technology object: " + planeSchema.getName());
             }
         }
-        int index = 0;
+
+        // count number of layers across all planes
+        int globalLayerCount = 0;
         for (PlaneSchema planeSchema : planeSchemas) {
-            planeSchema.setIndex(index);
-            index++;
+            globalLayerCount += planeSchema.getLayerNames().size();
         }
+        this.globalLayerCount = globalLayerCount;
+
+        // assign plane indices; initialize mappings using global layer indices as keys
+        int planeIndex = 0, globalLayerIndex = 0;
+        this.globalLayerIndexToPlaneIndex = new int[globalLayerCount];
+        this.globalLayerIndexToLocalLayerIndex = new int[globalLayerCount];
+        for (PlaneSchema planeSchema : planeSchemas) {
+            planeSchema.setIndex(planeIndex);
+            for (int i = 0; i < planeSchema.getLayerNames().size(); i++) {
+                globalLayerIndexToPlaneIndex[globalLayerIndex] = planeIndex;
+                globalLayerIndexToLocalLayerIndex[globalLayerIndex] = i;
+                globalLayerIndex++;
+            }
+            planeIndex++;
+        }
+
     }
 
     public String getId() {
         return id;
     }
 
-    public int getLayerCount() {
+//region planes
+
+    public int getPlaneCount() {
         return planeSchemas.size();
     }
 
+    public boolean isPlaneIndexValid(int planeIndex) {
+        return (planeIndex >= 0 && planeIndex < planeSchemas.size());
+    }
+
+    // TODO rename
+    public ImmutableList<PlaneSchema> getLayerSchemas() {
+        return planeSchemas;
+    }
+
+//endregion
+
+//region global layer indices
+
+    public int getGlobalLayerCount() {
+        return globalLayerCount;
+    }
+
+    public boolean isGlobalLayerIndexValid(int globalLayerIndex) {
+        return (globalLayerIndex >= 0 && globalLayerIndex < getGlobalLayerCount());
+    }
+
+    public void validateGlobalLayerIndex(int globalLayerIndex) {
+        if (!isGlobalLayerIndexValid(globalLayerIndex)) {
+            throw new IllegalArgumentException("invalid global layer index: " + globalLayerIndex);
+        }
+    }
+
+    public int getPlaneIndexForGlobalLayerIndex(int globalLayerIndex) {
+        validateGlobalLayerIndex(globalLayerIndex);
+        return globalLayerIndexToPlaneIndex[globalLayerIndex];
+    }
+
+    public int getLocalLayerIndexForGlobalLayerIndex(int globalLayerIndex) {
+        validateGlobalLayerIndex(globalLayerIndex);
+        return globalLayerIndexToLocalLayerIndex[globalLayerIndex];
+    }
+
+//endregion
+
+    // TODO remove
     public boolean isLayerIndexValid(int layerIndex) {
         return (layerIndex >= 0 && layerIndex < planeSchemas.size());
     }
 
+    // TODO remove
     public void validateLayerIndex(int layerIndex) {
         if (!isLayerIndexValid(layerIndex)) {
             throw new IllegalArgumentException("invalid layer index: " + layerIndex);
         }
-    }
-
-    public ImmutableList<PlaneSchema> getLayerSchemas() {
-        return planeSchemas;
     }
 
 }
