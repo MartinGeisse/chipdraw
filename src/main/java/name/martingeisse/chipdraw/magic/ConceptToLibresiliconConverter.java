2a881f7e6018ce6970e9cc7698eb95e9d4500679
Martin Geisse <martingeisse@googlemail.com>
2019-11-06 12:42:48 +0100

    use constants in converter

--- src/main/java/name/martingeisse/chipdraw/magic/ConceptToLibresiliconConverter.java
+++ src/main/java/name/martingeisse/chipdraw/magic/ConceptToLibresiliconConverter.java
@@ -1,6 +1,5 @@
 package name.martingeisse.chipdraw.magic;
 
-import com.google.common.collect.ImmutableList;
 import name.martingeisse.chipdraw.Design;
 import name.martingeisse.chipdraw.Plane;
 import name.martingeisse.chipdraw.technology.Technologies;
@@ -22,20 +21,21 @@ public final class ConceptToLibresiliconConverter {
             for (int y = 0; y < original.getHeight(); y++) {
 
                 // read original pixels
-                int originalWell = original.getPlanes().get(0).getCell(x, y);
-                int originalDiff = original.getPlanes().get(1).getCell(x, y);
-                int originalPoly = original.getPlanes().get(2).getCell(x, y);
-                int originalMetal1 = original.getPlanes().get(3).getCell(x, y);
-                int originalMetal2 = original.getPlanes().get(4).getCell(x, y);
-                int originalPad = original.getPlanes().get(5).getCell(x, y);
+                int originalWell = original.getPlanes().get(Technologies.Concept.PLANE_WELL).getCell(x, y);
+                int originalDiff = original.getPlanes().get(Technologies.Concept.PLANE_DIFF).getCell(x, y);
+                int originalPoly = original.getPlanes().get(Technologies.Concept.PLANE_POLY).getCell(x, y);
+                int originalMetal1 = original.getPlanes().get(Technologies.Concept.PLANE_METAL1).getCell(x, y);
+                int originalMetal2 = original.getPlanes().get(Technologies.Concept.PLANE_METAL2).getCell(x, y);
+                int originalPad = original.getPlanes().get(Technologies.Concept.PLANE_PAD).getCell(x, y);
 
                 // wells can be copied directly
-                converted.getPlanes().get(0).setCell(x, y, originalWell);
+                converted.getPlanes().get(Technologies.LibreSiliconMagicScmos.PLANE_WELL).setCell(x, y, originalWell);
 
                 // active has lots of different upwards contact types in Magic, all of which are represented by
                 // downwards contacts in metal1 in our "concept" tech
-                if (originalMetal1 == 0) {
+                if (originalMetal1 == Technologies.Concept.MATERIAL_LOCAL_METAL1_CONTACT) {
                     // concept metal1 has a downwards contact
+
                     if (originalPoly != Plane.EMPTY_CELL) {
                         // TODO converted.getPlanes().get(1).setCell(x, y, 1); // via12
                     }
@@ -48,33 +48,33 @@ public final class ConceptToLibresiliconConverter {
                     // no downwards contact
                 }
 
-
-                // TODO active
-                // converted.getPlanes().get(1).setCell(x, y, );
-
                 // LibreSilicon metal1 plane contains metal1/2 vias as "m2contact", which are in the metal2 plane in the "concept" tech
                 if (originalMetal1 == Plane.EMPTY_CELL) {
-                    if (originalMetal2 == 0) {
+                    if (originalMetal2 == Technologies.Concept.MATERIAL_LOCAL_METAL2_VIA12) {
                         throw new IncompatibilityException(x, y, "via12 without metal1");
                     } // else: empty
                 } else {
-                    if (originalMetal2 == 0) {
-                        converted.getPlanes().get(2).setCell(x, y, 1); // via12
+                    if (originalMetal2 == Technologies.Concept.MATERIAL_LOCAL_METAL2_VIA12) {
+                        converted.getPlanes().get(Technologies.LibreSiliconMagicScmos.PLANE_METAL1).setCell(x, y,
+                                Technologies.LibreSiliconMagicScmos.MATERIAL_LOCAL_METAL1_M2CONTACT);
                     } else {
-                        converted.getPlanes().get(2).setCell(x, y, 0); // metal1
+                        converted.getPlanes().get(Technologies.LibreSiliconMagicScmos.PLANE_METAL1).setCell(x, y,
+                                Technologies.LibreSiliconMagicScmos.MATERIAL_LOCAL_METAL1_METAL1);
                     }
                 }
 
                 // LibreSilicon metal2 plane contains pads as "pad", which are in the "pad" plane in the "concept" tech
                 if (originalMetal2 == Plane.EMPTY_CELL) {
-                    if (originalPad == 0) {
+                    if (originalPad == Technologies.Concept.MATERIAL_LOCAL_PAD_PAD) {
                         throw new IncompatibilityException(x, y, "pad without metal2");
                     } // else: empty
                 } else {
-                    if (originalPad == 0) {
-                        converted.getPlanes().get(3).setCell(x, y, 1); // pad
+                    if (originalPad == Technologies.Concept.MATERIAL_LOCAL_PAD_PAD) {
+                        converted.getPlanes().get(Technologies.LibreSiliconMagicScmos.PLANE_METAL2).setCell(x, y,
+                                Technologies.LibreSiliconMagicScmos.MATERIAL_LOCAL_METAL2_PAD);
                     } else {
-                        converted.getPlanes().get(3).setCell(x, y, 0); // metal2
+                        converted.getPlanes().get(Technologies.LibreSiliconMagicScmos.PLANE_METAL2).setCell(x, y,
+                                Technologies.LibreSiliconMagicScmos.MATERIAL_LOCAL_METAL2_METAL2);
                     }
                 }
 
