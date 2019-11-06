1b537667718d9f79ef54379492cf4333536460fa
Martin Geisse <martingeisse@googlemail.com>
2019-11-06 11:00:36 +0100

    changed the schema for the 'concept' technology; concept-to-LS converter, WIP

--- src/main/java/name/martingeisse/chipdraw/magic/ConceptToLibresiliconConverter.java
+++ src/main/java/name/martingeisse/chipdraw/magic/ConceptToLibresiliconConverter.java
@@ -5,16 +5,30 @@ import name.martingeisse.chipdraw.technology.Technologies;
 
 public final class ConceptToLibresiliconConverter {
 
-    private final Design conceptDesign;
+    private final Design original;
 
     public ConceptToLibresiliconConverter(Design conceptDesign) {
         if (conceptDesign.getTechnology() != Technologies.CONCEPT) {
             throw new IllegalArgumentException("input design for conversion must use 'concept' technology");
         }
-        this.conceptDesign = conceptDesign;
+        this.original = conceptDesign;
     }
 
     public Design convert() {
+        Design converted = new Design(Technologies.LIBRESILICON_MAGIC_SCMOS, original.getWidth(), original.getHeight());
+        for (int x = 0; x < original.getWidth(); x++) {
+            for (int y = 0; y < original.getHeight(); y++) {
+
+                // wells can be copied directly
+                converted.getPlanes().get(0).setCell(x, y, original.getPlanes().get(0).getCell(x, y));
+
+
+
+            }
+        }
+
+
+
         // TODO
         throw new UnsupportedOperationException();
     }
--- src/main/java/name/martingeisse/chipdraw/technology/Technologies.java
+++ src/main/java/name/martingeisse/chipdraw/technology/Technologies.java
@@ -4,16 +4,32 @@ import com.google.common.collect.ImmutableList;
 
 public final class Technologies {
 
+    /**
+     * Minimal pseudo-technology that is meant to design a cell concept. If all rules are respected (and those should
+     * be DRC-checked) then an actual technology-specific layout can in theory be generated from this one. This is
+     * "halfway SCMOS": The numeric values used in the DRC are still technology-specific, but the way the design is
+     * built (i.e. the planes and materials) are technology-independent.
+     *
+     * Vias and contacts are drawn as special materials in the *upper* connected plane. That is, well taps, diffusion
+     * contacts and poly contacts are drawn in the metal1 plane using a special "contact" material instead of the
+     * normal metal1 material. Metal1-to-metal2 vias are drawn in metal2 using a special via12 material, and so on.
+     * Using a material instead of a plane simplifies things (no DRC needed between the via and the upper connected
+     * plane), and using the upper connected plane instead of the lower one avoids complexity due to the lowest
+     * planes (wells / diffusion / poly) being multiple planes with multiple materials each.
+     */
     public static final Technology CONCEPT = buildConceptTechnology();
+
+    /**
+     * This closely resembles the LibreSilicon designs in Magic.
+     */
     public static final Technology LIBRESILICON_MAGIC_SCMOS = buildLibresiliconMagicScmos();
 
     private static Technology buildConceptTechnology() {
         PlaneSchema well = new PlaneSchema("well", ImmutableList.of("nwell", "pwell"));
         PlaneSchema diff = new PlaneSchema("diff", ImmutableList.of("ndiff", "pdiff"));
         PlaneSchema poly = new PlaneSchema("poly");
-        PlaneSchema contact = new PlaneSchema("contact");
-        PlaneSchema metal1 = new PlaneSchema("metal1");
-        ImmutableList<PlaneSchema> planeSchemas = ImmutableList.of(well, diff, poly, contact, metal1);
+        PlaneSchema metal1 = new PlaneSchema("metal1", ImmutableList.of("metal1", "contact"));
+        ImmutableList<PlaneSchema> planeSchemas = ImmutableList.of(well, diff, poly, metal1);
         return new Technology("concept", planeSchemas);
     }
 
