a21647dd081ec08f56313e94e2e5e1492374e739
Martin Geisse <martingeisse@googlemail.com>
2019-11-12 19:58:49 +0100

    refactoring: avoid plane indices, local material indices, global material indices. Use schema objects as indices instead.

--- src/main/java/name/martingeisse/chipdraw/design/Technologies.java
+++ src/main/java/name/martingeisse/chipdraw/design/Technologies.java
@@ -4,42 +4,6 @@ import com.google.common.collect.ImmutableList;
 
 public final class Technologies {
 
-    public static final class Concept {
-
-        public static final int PLANE_WELL = 0;
-        public static final int PLANE_DIFF = 1;
-        public static final int PLANE_POLY = 2;
-        public static final int PLANE_METAL1 = 3;
-        public static final int PLANE_METAL2 = 4;
-        public static final int PLANE_PAD = 5;
-
-        public static final int MATERIAL_LOCAL_WELL_NWELL = 0;
-        public static final int MATERIAL_LOCAL_WELL_PWELL = 1;
-        public static final int MATERIAL_LOCAL_DIFF_NDIFF = 0;
-        public static final int MATERIAL_LOCAL_DIFF_PDIFF = 1;
-        public static final int MATERIAL_LOCAL_POLY_POLY = 0;
-        public static final int MATERIAL_LOCAL_METAL1_CONTACT = 0;
-        public static final int MATERIAL_LOCAL_METAL1_METAL1 = 1;
-        public static final int MATERIAL_LOCAL_METAL2_VIA12 = 0;
-        public static final int MATERIAL_LOCAL_METAL2_METAL2 = 1;
-        public static final int MATERIAL_LOCAL_PAD_PAD = 0;
-
-        private Concept() {
-        }
-
-        private static Technology build() {
-            PlaneSchema well = new PlaneSchema("well", ImmutableList.of("nwell", "pwell"));
-            PlaneSchema diff = new PlaneSchema("diff", ImmutableList.of("ndiff", "pdiff"));
-            PlaneSchema poly = new PlaneSchema("poly");
-            PlaneSchema metal1 = new PlaneSchema("metal1", ImmutableList.of("contact", "metal1"));
-            PlaneSchema metal2 = new PlaneSchema("metal2", ImmutableList.of("via12", "metal2"));
-            PlaneSchema pad = new PlaneSchema("pad");
-            ImmutableList<PlaneSchema> planeSchemas = ImmutableList.of(well, diff, poly, metal1, metal2, pad);
-            return new Technology("concept", planeSchemas);
-        }
-
-    }
-
     /**
      * Minimal pseudo-technology that is meant to design a cell concept. If all rules are respected (and those should
      * be DRC-checked) then an actual technology-specific layout can in theory be generated from this one. This is
@@ -53,31 +17,81 @@ public final class Technologies {
      * plane), and using the upper connected plane instead of the lower one avoids complexity due to the lowest
      * planes (wells / diffusion / poly) being multiple planes with multiple materials each.
      */
-    public static final Technology CONCEPT = Concept.build();
+    public static final class Concept {
 
+        public static final Technology TECHNOLOGY;
+
+        public static final PlaneSchema PLANE_WELL;
+        public static final PlaneSchema PLANE_DIFF;
+        public static final PlaneSchema PLANE_POLY;
+        public static final PlaneSchema PLANE_METAL1;
+        public static final PlaneSchema PLANE_METAL2;
+        public static final PlaneSchema PLANE_PAD;
+
+        public static final Material MATERIAL_NWELL;
+        public static final Material MATERIAL_PWELL;
+        public static final Material MATERIAL_NDIFF;
+        public static final Material MATERIAL_PDIFF;
+        public static final Material MATERIAL_POLY;
+        public static final Material MATERIAL_CONTACT;
+        public static final Material MATERIAL_METAL1;
+        public static final Material MATERIAL_VIA12;
+        public static final Material MATERIAL_METAL2;
+        public static final Material MATERIAL_PAD;
+
+        private Concept() {
+        }
+
+        static {
+            PLANE_WELL = new PlaneSchema("well", ImmutableList.of("nwell", "pwell"));
+            MATERIAL_NWELL = PLANE_WELL.getMaterials().get(0);
+            MATERIAL_PWELL = PLANE_WELL.getMaterials().get(1);
+            PLANE_DIFF = new PlaneSchema("diff", ImmutableList.of("ndiff", "pdiff"));
+            MATERIAL_NDIFF = PLANE_DIFF.getMaterials().get(0);
+            MATERIAL_PDIFF = PLANE_DIFF.getMaterials().get(1);
+            PLANE_POLY = new PlaneSchema("poly");
+            MATERIAL_POLY = PLANE_POLY.getMaterials().get(0);
+            PLANE_METAL1 = new PlaneSchema("metal1", ImmutableList.of("contact", "metal1"));
+            MATERIAL_CONTACT = PLANE_METAL1.getMaterials().get(0);
+            MATERIAL_METAL1 = PLANE_METAL1.getMaterials().get(1);
+            PLANE_METAL2 = new PlaneSchema("metal2", ImmutableList.of("via12", "metal2"));
+            MATERIAL_VIA12 = PLANE_METAL2.getMaterials().get(0);
+            MATERIAL_METAL2 = PLANE_METAL2.getMaterials().get(1);
+            PLANE_PAD = new PlaneSchema("pad");
+            MATERIAL_PAD = PLANE_PAD.getMaterials().get(0);
+            TECHNOLOGY = new Technology("concept", ImmutableList.of(PLANE_WELL, PLANE_DIFF, PLANE_POLY, PLANE_METAL1, PLANE_METAL2, PLANE_PAD));
+        }
+
+    }
+
+    /**
+     * This closely resembles the LibreSilicon designs in Magic.
+     */
     public static final class LibreSiliconMagicScmos {
 
-        public static final int PLANE_WELL = 0;
-        public static final int PLANE_ACTIVE = 1;
-        public static final int PLANE_METAL1 = 2;
-        public static final int PLANE_METAL2 = 3;
+        public static final Technology TECHNOLOGY;
 
-        public static final int MATERIAL_LOCAL_WELL_NWELL = 0;
-        public static final int MATERIAL_LOCAL_WELL_PWELL = 1;
-        public static final int MATERIAL_LOCAL_ACTIVE_POLYSILICON = 0;
-        public static final int MATERIAL_LOCAL_ACTIVE_NDIFFUSION = 1;
-        public static final int MATERIAL_LOCAL_ACTIVE_PDIFFUSION = 2;
-        public static final int MATERIAL_LOCAL_ACTIVE_NTRANSISTOR = 3;
-        public static final int MATERIAL_LOCAL_ACTIVE_PTRANSISTOR = 4;
-        public static final int MATERIAL_LOCAL_ACTIVE_POLYCONTACT = 5;
-        public static final int MATERIAL_LOCAL_ACTIVE_NDCONTACT = 6;
-        public static final int MATERIAL_LOCAL_ACTIVE_PDCONTACT = 7;
-        public static final int MATERIAL_LOCAL_ACTIVE_NSUBSTRATENCONTACT = 8;
-        public static final int MATERIAL_LOCAL_ACTIVE_PSUBSTRATEPCONTACT = 9;
-        public static final int MATERIAL_LOCAL_METAL1_METAL1 = 0;
-        public static final int MATERIAL_LOCAL_METAL1_M2CONTACT = 1;
-        public static final int MATERIAL_LOCAL_METAL2_METAL2 = 0;
-        public static final int MATERIAL_LOCAL_METAL2_PAD = 1;
+        public static final PlaneSchema PLANE_WELL = 0;
+        public static final PlaneSchema PLANE_ACTIVE = 1;
+        public static final PlaneSchema PLANE_METAL1 = 2;
+        public static final PlaneSchema PLANE_METAL2 = 3;
+
+        public static final Material MATERIAL_NWELL = 0;
+        public static final Material MATERIAL_PWELL = 1;
+        public static final Material MATERIAL_POLYSILICON = 0;
+        public static final Material MATERIAL_NDIFFUSION = 1;
+        public static final Material MATERIAL_PDIFFUSION = 2;
+        public static final Material MATERIAL_NTRANSISTOR = 3;
+        public static final Material MATERIAL_PTRANSISTOR = 4;
+        public static final Material MATERIAL_POLYCONTACT = 5;
+        public static final Material MATERIAL_NDCONTACT = 6;
+        public static final Material MATERIAL_PDCONTACT = 7;
+        public static final Material MATERIAL_NSUBSTRATENCONTACT = 8;
+        public static final Material MATERIAL_PSUBSTRATEPCONTACT = 9;
+        public static final Material MATERIAL_METAL1 = 0;
+        public static final Material MATERIAL_M2CONTACT = 1;
+        public static final Material MATERIAL_METAL2 = 0;
+        public static final Material MATERIAL_PAD = 1;
 
         private LibreSiliconMagicScmos() {
         }
@@ -99,11 +113,6 @@ public final class Technologies {
         }
 
     }
-
-    /**
-     * This closely resembles the LibreSilicon designs in Magic.
-     */
-    public static final Technology LIBRESILICON_MAGIC_SCMOS = LibreSiliconMagicScmos.build();
 
     private Technologies() {
     }
