e9edc60955888805e1494cf7f1c4450e367e61d8
Martin Geisse <martingeisse@googlemail.com>
2019-12-25 08:43:30 +0100

    added v2/a3oi; liberty file generator now directly overwrites the old file instead of writing to System.out

--- pixel/src/main/java/name/martingeisse/chipdraw/pixel/tools/LibertyGeneratorMain.java
+++ pixel/src/main/java/name/martingeisse/chipdraw/pixel/tools/LibertyGeneratorMain.java
@@ -1,33 +1,48 @@
 package name.martingeisse.chipdraw.pixel.tools;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
 public class LibertyGeneratorMain {
 
-    public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("/*");
-        System.out.println(" delay model :       typ");
-        System.out.println(" check model :       typ");
-        System.out.println(" power model :       typ");
-        System.out.println(" capacitance model : typ");
-        System.out.println(" other model :       typ");
-        System.out.println("*/");
-        System.out.println("library(osu05_stdcells) {");
+    private static PrintWriter out;
 
-        printFunctionCell("Inverter", 28 * 70, "(!a)", "a");
-        printFunctionCell("Nand", 35 * 70, "(!(a b))", "a", "b");
-        printFunctionCell("Nand3", 42 * 70, "(!(a b c))", "a", "b", "c");
-        printFunctionCell("Nor", 35 * 70, "(!(a+b))", "a", "b");
-        printFunctionCell("Nor3", 42 * 70, "(!(a+b+c))", "a", "b", "c");
+    public static void main(String[] args) throws Exception {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("resource/7seg/own.lib")) {
+            try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.US_ASCII)) {
+                try (PrintWriter out = new PrintWriter(outputStreamWriter)) {
+                    LibertyGeneratorMain.out = out;
+                    out.println("");
+                    out.println("/*");
+                    out.println(" delay model :       typ");
+                    out.println(" check model :       typ");
+                    out.println(" power model :       typ");
+                    out.println(" capacitance model : typ");
+                    out.println(" other model :       typ");
+                    out.println("*/");
+                    out.println("library(osu05_stdcells) {");
 
-        printFunctionCell("Buffer", 35 * 70, "a", "a");
-        printFunctionCell("And", 49 * 70, "(a b)", "a", "b");
-        printFunctionCell("Or", 49 * 70, "(a+b)", "a", "b");
+                    printFunctionCell("Inverter", 28 * 70, "(!a)", "a");
+                    printFunctionCell("Nand", 35 * 70, "(!(a b))", "a", "b");
+                    printFunctionCell("Nand3", 42 * 70, "(!(a b c))", "a", "b", "c");
+                    printFunctionCell("Nor", 35 * 70, "(!(a+b))", "a", "b");
+                    printFunctionCell("Nor3", 42 * 70, "(!(a+b+c))", "a", "b", "c");
 
-        printFunctionCell("AndOrInvert", 42 * 70, "(!((a b)+c))", "a", "b", "c");
-        printFunctionCell("OrAndInvert", 42 * 70, "(!((a+b) c))", "a", "b", "c");
-        printFunctionCell("AndAndOrInvert", 49 * 70, "(!((a b)+(c d)))", "a", "b", "c", "d");
+                    printFunctionCell("Buffer", 35 * 70, "a", "a");
+                    printFunctionCell("And", 49 * 70, "(a b)", "a", "b");
+                    printFunctionCell("Or", 49 * 70, "(a+b)", "a", "b");
 
-        System.out.println("}");
+                    printFunctionCell("AndOrInvert", 42 * 70, "(!((a b)+c))", "a", "b", "c");
+                    printFunctionCell("OrAndInvert", 42 * 70, "(!((a+b) c))", "a", "b", "c");
+                    printFunctionCell("AndAndOrInvert", 49 * 70, "(!((a b)+(c d)))", "a", "b", "c", "d");
+                    printFunctionCell("And3OrInvert", 49 * 70, "(!((a b c)+d))", "a", "b", "c", "d");
+
+                    out.println("}");
+                }
+            }
+        }
     }
 
     private static void printFunctionCell(String name, int area, String function, String... inputPinNames) {
@@ -38,8 +53,8 @@ public class LibertyGeneratorMain {
     }
 
     private static void beginCell(String name, int area) {
-        System.out.println("\tcell(" + name + ") {");
-        System.out.println("\t\tarea: " + area + ";");
+        out.println("\tcell(" + name + ") {");
+        out.println("\t\tarea: " + area + ";");
     }
 
     private static void printInputPins(String... names) {
@@ -49,20 +64,20 @@ public class LibertyGeneratorMain {
     }
 
     private static void printInputPin(String name) {
-        System.out.println("\t\tpin(" + name + ") {");
-        System.out.println("\t\t\tdirection: input;");
-        System.out.println("\t\t}");
+        out.println("\t\tpin(" + name + ") {");
+        out.println("\t\t\tdirection: input;");
+        out.println("\t\t}");
     }
 
     private static void printOutputPin(String name, String function) {
-        System.out.println("\t\tpin(" + name + ") {");
-        System.out.println("\t\t\tdirection: output;");
-        System.out.println("\t\t\tfunction: \"" + function + "\";");
-        System.out.println("\t\t}");
+        out.println("\t\tpin(" + name + ") {");
+        out.println("\t\t\tdirection: output;");
+        out.println("\t\t\tfunction: \"" + function + "\";");
+        out.println("\t\t}");
     }
 
     private static void endCell() {
-        System.out.println("\t}");
+        out.println("\t}");
     }
 
 }
