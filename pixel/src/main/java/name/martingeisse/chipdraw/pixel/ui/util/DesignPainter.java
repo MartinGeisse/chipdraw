75f1cce43ddf8a537ac12287615b18b4344dc848
Martin Geisse <martingeisse@googlemail.com>
2020-02-14 22:04:05 +0100

    added a tool main to draw the standard cells as PNG images

--- pixel/src/main/java/name/martingeisse/chipdraw/pixel/ui/util/DesignPainter.java
+++ pixel/src/main/java/name/martingeisse/chipdraw/pixel/ui/util/DesignPainter.java
@@ -1,10 +1,8 @@
 package name.martingeisse.chipdraw.pixel.ui.util;
 
 import name.martingeisse.chipdraw.pixel.design.Design;
-import name.martingeisse.chipdraw.pixel.ui.MainWindow;
 import org.apache.commons.lang3.tuple.Pair;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -12,36 +10,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
+ * This class performs painting the design so it can be done outside a component.
  */
-public abstract class DesignPixelPanel extends JPanel {
+public abstract class DesignPainter {
 
     private static final int MACRO_GRID_SIZE = 7;
 
-    private final MainWindow mainWindow;
     private final Map<Pair<Integer, Integer>, Paint> paintCache = new HashMap<>();
 
-    public DesignPixelPanel(MainWindow mainWindow) {
-        this.mainWindow = mainWindow;
-    }
-
-    @Override
-    protected void paintComponent(Graphics _g) {
-        Graphics2D g = (Graphics2D) _g;
-        Design design = mainWindow.getCurrentDesign();
+    public void paintDesign(Graphics2D g, Design design, int zoom) {
 
         // get width / height / pixel size
-        int zoom = mainWindow.getZoom();
-        int panelWidth = getWidth();
-        int panelHeight = getHeight();
         int designDisplayWidth = design.getWidth() * zoom;
         int designDisplayHeight = design.getHeight() * zoom;
 
         // draw background
-        if (designDisplayWidth < panelWidth || designDisplayHeight < panelHeight) {
-            g.setColor(Color.LIGHT_GRAY);
-            g.fillRect(0, 0, panelWidth, panelHeight);
-        }
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, designDisplayWidth, designDisplayHeight);
 
